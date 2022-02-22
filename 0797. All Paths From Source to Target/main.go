502f262ed687a25adf4dcffc1a765cb5b643093e
enginewang <engine74396@gmail.com>
2022-02-23 00:23:24 +0800

    20220222

--- 0797. All Paths From Source to Target/main.go
+++ 0797. All Paths From Source to Target/main.go
@@ -3,25 +3,47 @@ package main
 import "fmt"
 
 // dfs
+//func allPathsSourceTarget(graph [][]int) [][]int {
+//	var dfs func(int, []int)
+//	var result [][]int
+//	dfs = func(i int, path []int) {
+//		path = append(path, i)
+//		if i == len(graph)-1 {
+//			// 务必要这样，直接path有问题
+//			ans := make([]int, len(path))
+//			copy(ans, path)
+//			result = append(result, ans)
+//			return
+//		}
+//		for _, id := range graph[i] {
+//			dfs(id, path)
+//		}
+//	}
+//	dfs(0, []int{})
+//	return result
+//}
+
+
+// 图的遍历
 func allPathsSourceTarget(graph [][]int) [][]int {
-	//visited := make([]bool, len(graph))
-	var dfs func(int, []int)
 	var result [][]int
-	dfs = func(i int, path []int) {
-		path = append(path, i)
-		if i == len(graph)-1 {
-			// 务必要这样，直接path有问题
-			ans := make([]int, len(path))
-			copy(ans, path)
-			result = append(result, ans)
-			return
-		}
-		for _, id := range graph[i] {
-			dfs(id, path)
-		}
-	}
-	dfs(0, []int{})
+	var path []int
+	traverse(graph, path, 0, &result)
 	return result
+}
+
+func traverse(graph [][]int, path []int, c int, result *[][]int)  {
+	path = append(path, c)
+	// 当前结点是最后一个，满足条件
+	if c == len(graph) - 1{
+		tmp := make([]int, len(path))
+		copy(tmp, path)
+		*result = append(*result, tmp)
+	}
+	for _, child := range graph[c] {
+		traverse(graph, path, child, result)
+	}
+	path = path[:len(path)-1]
 }
 
 func main() {
